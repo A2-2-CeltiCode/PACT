1234712ed3d29965698b638ec3443d2fe8c1e64f
Arcthuruss <73853221+Arcthuruss@users.noreply.github.com>
2024-10-14 16:03:32 +0200

    git migration

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-?>
+<html lang="fr-FR">
+<head>
+    <title>PACT</title>
+</head>
+<body>
+<header>
+    <h1>PACT</h1>
+</header>
+<main>
+    <?php
+
+    ?>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
+
