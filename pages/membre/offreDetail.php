7428cc5bbfba6ae6999db5ab811a74b8ff48a6f2
dwamlir <152194340+dwamlir@users.noreply.github.com>
2024-10-21 10:53:26 +0200

    fix chemin logo

--- pages/membre/offreDetail.php
+++ pages/membre/offreDetail.php
@@ -19,7 +19,7 @@
     <!-- En-tête du site avec le logo, la barre de recherche, la navigation et la langue -->
     <header class="entete">
         <div class="entete-logo">
-            <img src="./assets/icon/logo.svg" alt="Logo PACT">
+            <img src="./ressources/icon/logo.svg" alt="Logo PACT">
             <span class="texte-logo">PACT</span>
         </div>
         <div class="entete-recherche">
@@ -32,7 +32,7 @@
         </nav>
         <div class="entete-langue">
             <!-- Sélecteur de langue avec icône -->
-            <img src="assets/icon/logofr.svg" alt="Français">
+            <img src="ressources/icone/logofr.svg" alt="Français">
             <select name="langue" id="langue">
                 <option value="fr">Français</option>
                 <option value="en">English</option>
@@ -48,12 +48,12 @@
     <main>
         <div class="offre">
             <!-- Image principale de l'offre -->
-            <img class="image-offre" alt="Image offre" src="assets/cate.jpg" />
+            <img class="image-offre" alt="Image offre" src="ressources/cate.jpg" />
 
             <div class="description">
                 <h2>
                     <!-- Icône de restaurant avec texte de titre -->
-                    <img src="assets/icon/restaurant.svg" alt="Logo restaurant" style="vertical-align: middle; height: 40px;">
+                    <img src="ressources/icone/restaurant.svg" alt="Logo restaurant" style="vertical-align: middle; height: 40px;">
                     Saveur de Bretagne
                 </h2>
                 <!-- Description du restaurant -->
@@ -61,19 +61,19 @@
 
                 <h3>
                     <!-- Icône d'horloge et horaires -->
-                    <img src="assets/icon/horloge.svg" alt="Logo horloge" style="vertical-align: middle; height: 40px;">
+                    <img src="ressources/icone/horloge.svg" alt="Logo horloge" style="vertical-align: middle; height: 40px;">
                     11h-15h & 19h-23h
                 </h3>
 
                 <h3>
                     <!-- Icône de navigation et lien vers le site web -->
-                    <img src="assets/icon/naviguer.svg" alt="Logo site web" style="vertical-align: middle; height: 40px;">
+                    <img src="ressources/icone/naviguer.svg" alt="Logo site web" style="vertical-align: middle; height: 40px;">
                     <a href="https://www.lesfilsamaman.com/restaurants/rennes/?utm_source=google&utm_medium=organic&utm_campaign=mybusiness-website">Site du restaurant</a>
                 </h3>
 
                 <h3>
                     <!-- Icône de localisation et adresse -->
-                    <img src="assets/icon/localisateur.svg" alt="Logo position" style="vertical-align: middle; height: 40px;">
+                    <img src="ressources/icone/localisateur.svg" alt="Logo position" style="vertical-align: middle; height: 40px;">
                     Lannion (22300) 3 rue bidule
                 </h3>
 
@@ -92,11 +92,11 @@
             <div class="colonne-droite">
                 <div class="note_etoiles">
                     <!-- Icônes d'étoiles pour la note -->
-                    <img src="assets/icon/etoile_pleine.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
-                    <img src="assets/icon/etoile_pleine.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
-                    <img src="assets/icon/etoile_pleine.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
-                    <img src="assets/icon/etoile_mid.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
-                    <img src="assets/icon/etoile_vide.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
+                    <img src="ressources/icone/etoile_pleine.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
+                    <img src="ressources/icone/etoile_pleine.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
+                    <img src="ressources/icone/etoile_pleine.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
+                    <img src="ressources/icone/etoile_mid.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
+                    <img src="ressources/icone/etoile_vide.svg" alt="Logo étoile pleine" style="vertical-align: middle;">
                     <!-- (répéter pour le nombre d'étoiles pleines et vides nécessaires) -->
                 </div>
                 <h3>€€€</h3>
@@ -109,7 +109,7 @@
                         <div>
                             <h2>Serge Sauvion</h2>
                             <p>
-                                <img src="assets/icon/localisateur.svg" alt="Icon localisation" style="vertical-align: middle;">
+                                <img src="ressources/icone/localisateur.svg" alt="icone localisation" style="vertical-align: middle;">
                                 Lannion
                             </p>
                         </div>
@@ -117,16 +117,16 @@
 
                     <div class="dessous-carte">
                         <div class="email">
-                            <img src="assets/icon/lettre.svg" alt="Icon lettre" style="vertical-align: middle;">
+                            <img src="ressources/icone/lettre.svg" alt="icone lettre" style="vertical-align: middle;">
                             <a href="mailto:serge@gmail.com">serge@gmail.com</a>
                         </div>
                         <div class="telephone">
-                            <img src="assets/icon/telephone.svg" alt="Icon téléphone" style="vertical-align: middle;">
+                            <img src="ressources/icone/telephone.svg" alt="icone téléphone" style="vertical-align: middle;">
                             <span>(671) 555-0110</span>
                         </div>
                         <!-- Bouton d'envoi de mail -->
                         <a class="bouton" href="mailto:serge@gmail.com">
-                            <img src="assets/icon/lettre.svg" alt="Icon lettre" style="vertical-align: middle;">
+                            <img src="ressources/icone/lettre.svg" alt="icone lettre" style="vertical-align: middle;">
                             Envoyer un Mail
                         </a>
                     </div>
@@ -147,12 +147,12 @@
         </div>
         <div class="icones-reseaux-sociaux">
             <!-- Icônes de réseaux sociaux : Facebook, Twitter, Instagram -->
-            <a href="https://www.facebook.com/?locale=fr_FR"><img src="assets/icon/facebook.svg" alt="Icon facebook" style="vertical-align: middle;"></a>
-            <a href="https://x.com/TripEnArvorPACT"><img src="assets/icon/twitter.svg" alt="Icon X" style="vertical-align: middle;"></a>
-            <a href="https://www.instagram.com/pactlannion/"><img src="assets/icon/instagram.svg" alt="Icon instagram" style="vertical-align: middle;"></a>
+            <a href="https://www.facebook.com/?locale=fr_FR"><img src="ressources/icone/facebook.svg" alt="Icon facebook" style="vertical-align: middle;"></a>
+            <a href="https://x.com/TripEnArvorPACT"><img src="ressources/icone/twitter.svg" alt="Icon X" style="vertical-align: middle;"></a>
+            <a href="https://www.instagram.com/pactlannion/"><img src="ressources/icone/instagram.svg" alt="Icon instagram" style="vertical-align: middle;"></a>
         </div>
         <p>© 2024 PACT, Inc.</p>
-        <a href="#entete" class="remonter-page"><img src="assets/icon/fleche.svg" alt="Icon fleche" style="vertical-align: middle;"></a>
+        <a href="#entete" class="remonter-page"><img src="ressources/icone/fleche.svg" alt="Icon fleche" style="vertical-align: middle;"></a>
     </footer>
 </body>
 </html>
