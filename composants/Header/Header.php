040207d20a57b3d93fa7c7d649b84d9a33da08e2
Hazard Kim <kim.hazard13@gmail.com>
2024-10-21 15:01:13 +0200

    fix header

--- composants/Header/Header.php
+++ composants/Header/Header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @class Header
  * @brief Classe utilitaire pour générer l'entête (header) HTML du site.
@@ -13,44 +14,38 @@ class Header
      */
     public static function render()
     {
-            echo '<link rel="stylesheet" href="../../../composants/Header/Header.css">';
+        echo '<link rel="stylesheet" href="../../../composants/Header/Header.css">';
 
         echo '
-        <header id="entete" class="entete">
-            <div class="entete-logo">
-                <!-- Logo de l\'entreprise avec texte -->
-                <img src="../../../ressources/icone/logo.svg" alt="Logo PACT">
-                <span class="texte-logo">PACT</span>
-            </div>
-            <div class="entete-recherche">
-                <!-- Composant d\'entrée pour la recherche -->
-        ';
-        Input::render(
-            class: 'champ-recherche', 
-            type: 'text', 
-            placeholder: 'Entrez une localisation...'
-        );
-
+        <header>
+    <div>
+        <img src="../../../ressources/icone/logo.svg" alt="Logo PACT">
+        <span>PACT</span>
+    </div>
+    <div>';
+        Input::render(placeholder: 'Entrez une localisation...');
         echo '
-            </div>
-            <nav class="entete-navigation">
-                <!-- Liens de navigation pour la page d\'accueil et les offres -->
-                <a href="index.php">Accueil</a>
-                <a href="offre.php">Offres</a>
-            </nav>
-            <div class="entete-langue">
-                <img id="logo-langue" src="../../../ressources/icone/logofr.svg" alt="Français">
-                <select id="selecteur-langue">
-                    <option value="fr">Français</option>
-                    <option value="en">English</option>
-                </select>
-            </div>
-            <div class="entete-connexion">
-                <a href="connexionComptePro.php" class="lien-bouton">
-                    <button>S\'inscrire / Se Connecter</button>
-                </a>
-            </div>
-        </header>
+    </div>
+    <nav>
+        <!-- Liens de navigation pour la page d\'accueil et les offres -->
+        <a href="index.php">Accueil</a>
+        <a href="offre.php">Offres</a>
+    </nav>
+    <div class="entete-langue">
+        <img id="logo-langue" src="../../../ressources/icone/logofr.svg" alt="Français">
+        <label for="selecteur-langue"></label>
+        <select id="selecteur-langue">
+            <option value="fr">Français</option>
+            <option value="en">English</option>
+        </select>
+    </div>
+    <div>
+        <a href="connexionComptePro.php" class="lien-bouton">
+            <button>S\'inscrire / Se Connecter</button>
+        </a>
+    </div>
+    <script src="../../../composants/Header/Header.js"></script>
+</header>
         <script src="../../../composants/Header/Header.js"></script>';
     }
 }
