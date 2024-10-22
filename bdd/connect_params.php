31ce8b8474e5d1f30f0bc094cfcf0d3a82d3ccdf
Hazard Kim <kim.hazard13@gmail.com>
2024-10-22 15:07:46 +0200

    test bdd

--- bdd/connect_params.php
+++ bdd/connect_params.php
@@ -1,6 +1,18 @@
 <?php
-$server = 'postgresdb,';
+// param server
+/*
+$server = 'postgresdb';
 $driver = 'pgsql';
 $dbname = 'sae';
 $user = 'sae';
 $pass = 'linc-keRRy-gor1lles';
+//*/
+
+// param local
+
+$server = 'servbdd.iutlan.etu.univ-rennes1.fr';
+$driver = 'pgsql';
+$dbname = 'pg_khazard';
+$user = 'khazard';
+$pass = 'BB1414cc7878ee11bb33-_-_';
+//*/
