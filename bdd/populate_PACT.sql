b7e742a82e8556a44f22e88726570fa74da6dcbb
Cyprien ALBERT <cpe.albert2@gmail.com>
2024-10-23 11:13:48 +0200

    BDD ajouté

--- bdd/populate_PACT.sql
+++ bdd/populate_PACT.sql
@@ -19,43 +19,54 @@ INSERT INTO _adresse (codePostal, ville, nomRue, numRue, numTel) VALUES
 (13001, 'Marseille', 'Rue de la Canebière', '3', '+33 4 91 23 45 67');
 
 -- Peupler la table _compte
-INSERT INTO _compte (login, mdp, email, codePostal, ville) VALUES
-('user1', 'password1', 'user1@example.com', 75001, 'Paris'),
-('user2', 'password2', 'user2@example.com', 69001, 'Lyon'),
-('user3', 'password3', 'user3@example.com', 13001, 'Marseille');
+INSERT INTO _compte (mdp, email, idAdresse) VALUES
+('password1', 'user1@example.com', 1),
+('password2', 'user2@example.com', 2),
+('password3', 'user3@example.com', 3);
 
 -- Peupler la table _compteMembre
-INSERT INTO _compteMembre (idCompte, prenom, nom) VALUES
-(1, 'Jean', 'Dupont');
+INSERT INTO _compteMembre (idCompte,login, prenom, nom) VALUES
+(1, 'user1',  'Jean','Dupont');
 
 -- Peupler la table _comptePro
 INSERT INTO _comptePro (idCompte, denominationSociale, raisonSocialePro, banqueRib) VALUES
 (2, 'Société A', 'Entreprise A', 'FR7612345678901234567890123'),
 (3, 'Société B', 'Entreprise B', 'FR7612345678901234567890124');
 
+INSERT INTO _compteProPrive (idCompte,numSiren) VALUES
+(2, 'numerosiren1');
+
+INSERT INTO _compteProPublic (idCompte) VALUES
+(3);
+
 -- Peupler la table _offre
-INSERT INTO _offre (idCompte, nomOption, nomForfait, titre, description, descriptionDetaillee, siteInternet) VALUES
-(2,'Aucune', 'Gratuite', 'Visite de Paris', 'Découvrez les merveilles de Paris', 'Une visite guidée de 2 heures', 'http://example.com/paris'),
-(2,'En relief', 'Standard', 'Spectacle à Lyon', 'Profitez d''un spectacle spectaculaire', 'Marionnettes et tours de cartes bluffant !', 'http://example.com/lyon'),
-(3,'A la une', 'Premium', 'Parc d''Angers', 'Parc incroyable', 'Parc proposant des attractions insane', 'http://example.com/angers');
+INSERT INTO _offre (idCompte, nomOption, nomForfait, titre, description, descriptionDetaillee, siteInternet, estEnLigne,idAdresse) VALUES
+(2,'Aucune', 'Gratuite', 'Visite de Paris', 'Découvrez les merveilles de Paris', 'Une visite guidée de 2 heures', 'http://example.com/paris', TRUE, 1),
+(2,'En relief', 'Standard', 'Spectacle à Lyon', 'Profitez d''un spectacle spectaculaire', 'Marionnettes et tours de cartes bluffant !', 'http://example.com/lyon', FALSE, 2),
+(3,'A la une', 'Premium', 'Parc de Marseille', 'Parc incroyable', 'Parc proposant des attractions insane', 'http://example.com/angers', TRUE, 3);
 
 -- Peupler la table _image
 INSERT INTO _image (idOffre, nomImage) VALUES
-(1, 'paris.jpg'),
-(2, 'lyon.jpg'),
-(3, 'angers.jpg');
+(3, 'paris.jpg'),
+(1, 'lyon.jpg'),
+(2, 'marseille.jpg'),
+(3, 'plan-parc.jpg');
 
 -- Peupler la table _spectacle
 INSERT INTO _spectacle (idOffre, nomCategorie, tempsEnMinutes, valPrix, capacite) VALUES
 (1, 'Spectacle', 120, 40, 20);
 
 -- Peupler la table _parcAttractions
-INSERT INTO _parcAttractions (idOffre, nomCategorie, valPrix, planParc, nbAttractions, ageMin) VALUES
-(3, 'Parc d''attractions', 60, 'map_park.jpg', 10, 6);
+INSERT INTO _parcAttractions (idOffre, nomCategorie, valPrix, idImage, nbAttractions, ageMin) VALUES
+(3, 'Parc d''attractions', 60, 4, 10, 6);
 
 -- Peupler la table _visitE
 INSERT INTO _visite (idOffre, valPrix, tempsEnMinutes, nomCategorie, estGuidee) VALUES
 (2, 20, 60, 'Visite', TRUE);
+
+INSERT INTO _guideeVisite (IdOffre,nomLangage) VALUES
+(2,'Français'),
+(2,'Espagnol');
 
 -- Peupler la table _possedeSpectacle
 INSERT INTO _possedeSpectacle (idOffre, nomTag) VALUES
