58b6e738903b7ced42413f7cc40ca8742ce10345
Cyprien ALBERT <cpe.albert2@gmail.com>
2024-10-25 15:25:41 +0200

    update modifierOffre

--- html/pages/pro/modifierOffre/modifierOffre.php
+++ html/pages/pro/modifierOffre/modifierOffre.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//error_reporting(E_ALL ^ E_WARNING);
+error_reporting(E_ALL ^ E_WARNING);
 
 require_once $_SERVER["DOCUMENT_ROOT"] .  "/connect_params.php";
 $dbh = new PDO("$driver:host=$server;dbname=$dbname", $dbuser, $dbpass);
