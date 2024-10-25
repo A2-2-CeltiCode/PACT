9009cb6586619357bcda1839d7d576529d577792
Arcthuruss <73853221+Arcthuruss@users.noreply.github.com>
2024-10-25 13:30:30 +0200

    fix erreur sur les redirects index.php

--- html/index.php
+++ html/index.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
-!isset($_SESSION['idCompte']) ? header("Location: /pages/pro/accueil/accueil.php") : header("Location: /pages/visiteur/accueil/accueil.php");
+isset($_SESSION['idCompte']) ? header("Location: /pages/pro/accueil/accueil.php") : header("Location: /pages/visiteur/accueil/accueil.php");
 exit;
