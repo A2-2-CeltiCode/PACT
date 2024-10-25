8dae35922528010835959aad8934a6dc0c2a201e
Cyprien ALBERT <cpe.albert2@gmail.com>
2024-10-25 16:07:18 +0200

    s

--- html/pages/pro/modifierOffre/modifierOffre.php
+++ html/pages/pro/modifierOffre/modifierOffre.php
@@ -146,8 +146,8 @@ $typePromotion = $vueOffre["nomoption"];
 
 <body>
     <form class="info-display" id="myForm" method="post" action="confirmationModificationOffre.php" enctype="multipart/form-data">
-        <input type="hidden" name="idOffre" value="<?php echo $idoffre["idOffre"]; ?>">
-        <input name="typeOffre" type="hidden" value=<?php $$typeOffre["nomcategorie"] ?>>
+        <input type="hidden" name="idOffre" value="<?php echo htmlspecialchars($idoffre['idOffre']); ?>">
+        <input name="typeOffre" type="hidden" value=<?php $typeOffre["nomcategorie"] ?>>
         <?php
         
         ?>
