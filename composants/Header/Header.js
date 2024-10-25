690244815575c794f7293fa16b21e6bea7e77f7d
Hazard Kim <kim.hazard13@gmail.com>
2024-10-25 08:57:09 +0200

    supression des fichiers inutiles

--- composants/Header/Header.js
+++
@@ -1,4 +0,0 @@
-let selecteurLangue = document.getElementById("selecteur-langue");
-let logoLangue = document.getElementById("logo-langue");
-
-selecteurLangue.addEventListener("input", (e) => { logoLangue.src = `../../../ressources/icone/logo${e.target.value}.svg` });
