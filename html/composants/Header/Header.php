4307f0c25d98f62de79ab7bbfbf042006ae8bd3b
Skeltan <leguern.alexis2005@gmail.com>
2024-10-25 14:50:43 +0200

    encore fix liens

--- html/composants/Header/Header.php
+++ html/composants/Header/Header.php
@@ -109,7 +109,7 @@ class Header
             echo '<a href="offre.php">Offres</a>';
             echo '<a href="offre.php">Favoris</a>';
         } elseif ($type == HeaderType::Pro) {
-            echo '<a href="/pages/pro/listeOffre/listeOffre.php">Mes Offres</a>';
+            echo '<a href="/pages/pro/listeOffres/listeOffres.php">Mes Offres</a>';
             echo '<a href="/pages/pro/creerOffre/creerOffre.php">Créer une Offre</a>';
         }
         echo '</nav>';
