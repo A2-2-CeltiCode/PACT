062d1e43fbd2f55ca9099a9dba29c4537b095303
dwamlir <riwan.derouesne@etudiants.univ-rennes1.fr>
2024-11-26 11:23:49 +0100

    fix

--- html/pages/pro/consulterComptePro/consulterComptePro.php
+++ html/pages/pro/consulterComptePro/consulterComptePro.php
@@ -72,19 +72,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Compte</title>
     <link rel="stylesheet" href="consulterComptePro.css">
-    <script>
-        // Fonction pour activer la modification des champs
-        function activerModification() {
-            // Rendre tous les champs modifiables
-            document.querySelectorAll('.editable').forEach(function (element) {
-                element.removeAttribute('readonly');
-                element.style.backgroundColor = '#f0f0f0'; // Indiquer visuellement que le champ est éditable
-            });
-
-            // Afficher le bouton "Enregistrer"
-            document.getElementById('btnEnregistrer').style.display = 'block';
-        }
-    </script>
+    <script src="consulterComptePro.js"></script>
 </head>
 
 <body>
