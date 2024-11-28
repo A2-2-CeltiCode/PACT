e30f2798269bebc8943c6fe6ccc2889a11d547d1
Hazard Kim <kim.hazard13@gmail.com>
2024-11-28 12:23:21 +0100

    ajout du template de la facture

--- html/pages/pro/facture/creationFacture.php
+++ html/pages/pro/facture/creationFacture.php
@@ -31,17 +31,96 @@ try {
 
 <body>
     <main>
-        <section>
-            <p>Vendeur</p>
-            <p>Adresse de fou</p>
-            <p>31 Rue de ton père le chauve</p>
-        </section>
-        <section>
-            <p>Client</p>
-            <p>Le tonton bandeur</p>
-            <p>24 impasse des vierges plus vierges</p>
-        </section>
+        <table class="partie">
+            <tr>
+                <th>Vendeur</th>
+                <td>Mon Entreprise</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>22 Avenue des Martins 4450 Ville</td>
+        </table>
+        <table class="partie">
+            <tr>
+                <th>Client</th>
+                <td>Nom du Client</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>55 Avenue des Clients</td>
+        </table>
 
+        <table class="info-facture">
+            <tr>
+                <th>Date d'émission</th>
+                <th>Numéro de facture</th>
+                <th>Désignation de l'offre</th>
+                <th>Date de la prestation de services (mois concerné)</th>
+                <th>Date d'échéance du règlement</th>
+            </tr>
+            <tr>
+                <td>21.05.2000</td>
+                <td>36</td>
+                <td>L'offre de Caca</td>
+                <td>40.06.0000</td>
+                <td>4 jours</td>
+            </tr>
+        </table>
+        <table class="tarifs-facture" cellspacing="0" cellpadding="0">
+            <tr>
+                <th>Nom du service</th>
+                <th>Quantité du service</th>
+                <th>Abonnement HT</th>
+                <th>Abonnement TTC</th>
+                <th>Option activée HT</th>
+                <th>Option activée TTC</th>
+            </tr>
+            <tr>
+                <td>Service de caca</td>
+                <td>4 caca</td>
+                <td>15€</td>
+                <td>18€</td>
+                <td>2€</td>
+                <td>5€</td>
+            </tr>
+            <tr>
+                <td>Service de caca</td>
+                <td>4 caca</td>
+                <td>15€</td>
+                <td>18€</td>
+                <td>2€</td>
+                <td>5€</td>
+            </tr>
+            <tr>
+                <td>Service de caca</td>
+                <td>4 caca</td>
+                <td>15€</td>
+                <td>18€</td>
+                <td>2€</td>
+                <td>5€</td>
+            </tr>
+            <tr>
+                <td>Service de caca</td>
+                <td>4 caca</td>
+                <td>15€</td>
+                <td>18€</td>
+                <td>2€</td>
+                <td>5€</td>
+            </tr>
+        </table>
+        <table class="total">
+        <tr>
+                <th>Total HT</th>
+                <td>100€</td>
+            </tr>            <tr>
+                <th>Total TVA</th>
+                <td>20€</td>
+            </tr>
+            <tr>
+                <th>Total TTC</t>
+                <td>120€</td>
+            </tr>
+        </table>
     </main>
 </body>
 
