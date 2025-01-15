91d204ae5ba167928837eb3834fbf39f4ecb6a98
dwamlir <riwan.derouesne@etudiants.univ-rennes1.fr>
2025-01-15 08:50:26 +0100

    liste facture update

--- html/pages/pro/listeFacture/renderFacture.php
+++ html/pages/pro/listeFacture/renderFacture.php
@@ -3,22 +3,17 @@ session_start();
 error_reporting(0);
 require_once $_SERVER["DOCUMENT_ROOT"] . "/pages/pro/facture/creationFacture.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/pages/pro/listeFacture/calculJoursEnLigne.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/pages/pro/listeFacture/calculJoursOption.php";
 include $_SERVER["DOCUMENT_ROOT"] . '/connect_params.php';
-
 $idFacture = isset($_GET['idfacture']) ? $_GET['idfacture'] : null;
 $idOffre = isset($_GET['idoffre']) ? $_GET['idoffre'] : null;
 $mois = isset($_GET['mois']) ? $_GET['mois'] : null;
 
 if ($idFacture && $idOffre) {
     $dbh = new PDO("$driver:host=$server;dbname=$dbname", $dbuser, $dbpass);
-    
     $nbJours=calculerJoursEnLigne($dbh, $idOffre, $mois);
-    $nbjoursOption=calculerJoursOption($dbh, $idOffre, $mois);
-    $facture = new Facture($idOffre, 1, $_SESSION['idCompte'], $idFacture,$nbJours,$nbjoursOption, $dbh);
+    $facture = new Facture($idOffre, 1, $_SESSION['idCompte'], $idFacture,$nbJours, $dbh);
     echo $facture->render();
 } else {
     echo "Facture non trouvée.";
 }
 ?>
-
