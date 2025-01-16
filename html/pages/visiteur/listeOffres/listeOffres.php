d4b7c93682b1a99451d71cf9a7c5199f827ef3c6
BalaiDuckman2 <raf29450@gmail.com>
2025-01-16 10:45:27 +0100

    lesgong

--- html/pages/visiteur/listeOffres/listeOffres.php
+++ html/pages/visiteur/listeOffres/listeOffres.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-/*if (isset($_SESSION['idCompte']) && $_SESSION['typeUtilisateur'] == "pro") {
-    header("Location: /pages/pro/listeOffres/listeOffres.php");
-} elseif (!isset($_SESSION['idCompte'])) {
-    header("Location: /pages/visiteur/accueil/accueil.php");
-}*/
 use composants\Button\ButtonType;
-error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED);
 use \composants\Select\Select;
 use \composants\CheckboxSelect\CheckboxSelect;
 use \composants\Checkbox\Checkbox;
@@ -25,62 +19,53 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/trie/barreTrieMembre.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/composants/Header/Header.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/composants/Footer/Footer.php";
 
-
 // Connexion à la base de données
 include $_SERVER["DOCUMENT_ROOT"] . '/connect_params.php';
 $pdo = new PDO("$driver:host=$server;port=5432;dbname=$dbname", $dbuser, $dbpass);
 
 // Récupération des paramètres de la requête
-$sort = isset($_GET['sort']) ? $_GET['sort'] : 'idoffre DESC';
-$titre = isset($_GET['titre']) ? $_GET['titre'] : '';
-$localisation = isset($_GET['localisation']) ? $_GET['localisation'] : '';
-$minPrix = isset($_GET['minPrix']) ? $_GET['minPrix'] : null;
-$maxPrix = isset($_GET['maxPrix']) ? $_GET['maxPrix'] : null;
-$etat= isset($_GET['etat']) ? $_GET['etat'] : 'ouvertetferme';
-$ouverture = isset($_GET['ouverture']) ? $_GET['ouverture'] : null;
-$fermeture = isset($_GET['fermeture']) ? $_GET['fermeture'] : null;
-$trie = isset($_GET['trie']) ? $_GET['trie'] : 'idoffre DESC';
-$query = "SELECT * FROM offres WHERE 1=1";
-$params = [];
-
+$sort = $_GET['sort'] ?? 'idoffre DESC';
+$titre = $_GET['titre'] ?? '';
+$localisation = $_GET['localisation'] ?? '';
+$minPrix = $_GET['minPrix'] ?? null;
+$maxPrix = $_GET['maxPrix'] ?? null;
+$etat = $_GET['etat'] ?? 'ouvertetferme';
+$ouverture = $_GET['ouverture'] ?? null;
+$fermeture = $_GET['fermeture'] ?? null;
+$trie = $_GET['trie'] ?? 'idoffre DESC';
 $nomcategories = isset($_GET['nomcategorie']) ? explode(',', $_GET['nomcategorie']) : ['Tout'];
-$gamme = isset($_GET['option']) ? explode(',', $_GET['option']) : null;
+$option = isset($_GET['option']) ? explode(',', $_GET['option']) : [];
+$note = $_GET['note'] ?? null;
 
-if (!empty($_GET['nomcategorie'])) {
-    $categoriesPlaceholders = implode(',', array_fill(0, count($nomcategories), '?'));
-    $query .= " AND nomcategorie IN ($categoriesPlaceholders)";
-    $params = array_merge($params, $nomcategories);
-}
 // Récupération des résultats
-$resultats = getOffres($pdo, $trie, $minPrix, $maxPrix, $titre, $nomcategories, $ouverture, $fermeture, $localisation,$etat,$status,$idCompte,$note,$gamme);
+$resultats = getOffres($pdo, $trie, $minPrix, $maxPrix, $titre, $nomcategories, $ouverture, $fermeture, $localisation, $etat, null, null, $note, $option);
+
 // Vérifiez si la requête est une requête AJAX
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-    isset($_SESSION["idCompte"])?Header::render(type: HeaderType::Member):Header::render();
     $offres = [];
     foreach ($resultats as $item) {
         $sql = 'SELECT denominationsociale FROM pact._comptepro WHERE idcompte = :idcompte';
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindValue(':idcompte', $item['idcompte'], PDO::PARAM_INT);
-            $stmt->execute();
-            $proDetails = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':idcompte', $item['idcompte'], PDO::PARAM_INT);
+        $stmt->execute();
+        $proDetails = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            if ($item['moynotes'] == null) {
-                $item['moynotes'] = 0;
-            }
-            $item['nomgamme'] = $item['nomgamme'] ?? 'test';
-            $item['valprix'] = $item['valprix'] ?? 'test';
-            $offre = new Offre($item['titre'], $item['nomcategorie'], "o", $item['nomimage'], "o", $item['idoffre'], $item['tempsenminutes'],$item['moynotes'],$item['nomoption'],$item['heureouverture'],$item['heurefermeture'],$item['valprix'],$item['nomgamme']);
+        if ($item['moynotes'] == null) {
+            $item['moynotes'] = 0;
+        }
+        $item['nomgamme'] = $item['nomgamme'] ?? 'test';
+        $item['valprix'] = $item['valprix'] ?? 'test';
+        $offre = new Offre($item['titre'], $item['nomcategorie'], "o", $item['nomimage'], "o", $item['idoffre'], $item['tempsenminutes'], $item['moynotes'], $item['nomoption'], $item['heureouverture'], $item['heurefermeture'], $item['valprix'], $item['nomgamme']);
         $offres[] = (string)$offre;
     }
     $nombreOffres = count($offres);
     echo json_encode(['offres' => $offres, 'nombreOffres' => $nombreOffres]);
-    exit; 
+    exit;
 }
 
 // Si ce n'est pas une requête AJAX, inclure le HTML complet
 ?>
 <?php Header::render(HeaderType::Guest); ?>
-
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -92,51 +77,47 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     <link rel="stylesheet" href="listeOffre.js">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <script src="../../../trie/trieGeneral.js"></script>
-    
 </head>
 <body>
 <div id="pageOverlay" class="page-overlay"></div>
 
+<input type="hidden" name="status" value="null" />
+<!-- Nouveau bouton pour afficher/masquer barretrie -->
 
-    <input type="hidden" name="status" value="null" />
-    <!-- Nouveau bouton pour afficher/masquer barretrie -->
-    
+<div id="barretrie">
+<?php
+Trie::render($sort, $titre, $localisation, $minPrix, $maxPrix, $ouverture, $fermeture, $nomcategories);
+?>
+</div>
+<br>
 
-    <div id="barretrie">
+<div id="nombreOffres">
+    <p>Nombre d'offres affichées : <?php echo count($resultats); ?></p>
+</div>
+<div id="resultats" class="offres-container">
+
+    <!-- Affichage des résultats -->
     <?php
-    Trie::render($sort, $titre, $localisation, $minPrix, $maxPrix, $ouverture, $fermeture, $nomcategories);
-    ?>
-    </div>
-    <br>
+    foreach ($resultats as $item) {
+        $sql = 'SELECT denominationsociale FROM pact._comptepro WHERE idcompte = :idcompte';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':idcompte', $item['idcompte'], PDO::PARAM_INT);
+        $stmt->execute();
+        $proDetails = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    <div id="nombreOffres">
-        <p>Nombre d'offres affichées : <?php echo count($resultats); ?></p>
-    </div>
-    <div id="resultats" class="offres-container">
-   
-        <!-- Affichage des résultats -->
-        <?php
-        foreach ($resultats as $item) {
-            $sql = 'SELECT denominationsociale FROM pact._comptepro WHERE idcompte = :idcompte';
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindValue(':idcompte', $item['idcompte'], PDO::PARAM_INT);
-            $stmt->execute();
-            $proDetails = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if ($item['moynotes'] == null) {
-                $item['moynotes'] = 0;
-            }
-            $item['nomgamme'] = $item['nomgamme'] ?? 'test';
-            $item['valprix'] = $item['valprix'] ?? 'test';
-            
-            $offre = new Offre($item['titre'], $item['nomcategorie'], $item['ville'], $item['nomimage'], $proDetails['denominationsociale'], $item['idoffre'], $item['tempsenminutes'],$item['moynotes'],$item['nomoption'],$item['heureouverture'],$item['heurefermeture'],$item['valprix'],$item['nomgamme']);
-            echo $offre;
+        if ($item['moynotes'] == null) {
+            $item['moynotes'] = 0;
         }
-        ?>
-    </div>
+        $item['nomgamme'] = $item['nomgamme'] ?? 'test';
+        $item['valprix'] = $item['valprix'] ?? 'test';
 
-   
-    <script src="listeOffre.js"></script>
+        $offre = new Offre($item['titre'], $item['nomcategorie'], $item['ville'], $item['nomimage'], $proDetails['denominationsociale'], $item['idoffre'], $item['tempsenminutes'], $item['moynotes'], $item['nomoption'], $item['heureouverture'], $item['heurefermeture'], $item['valprix'], $item['nomgamme']);
+        echo $offre;
+    }
+    ?>
+</div>
+
+<script src="listeOffre.js"></script>
 </body>
 <?php Footer::render(FooterType::Guest); ?>
 </html>
--- html/trie/trieGeneral.js
+++ html/trie/trieGeneral.js
@@ -1,30 +1,18 @@
 function rechercher() {
   const searchInput = document.querySelector('input[name="titre"]').value;
-  const localisationInput = document.querySelector(
-    'input[name="localisation"]'
-  ).value;
+  const localisationInput = document.querySelector('input[name="localisation"]').value;
   const minPrixInput = document.querySelector('input[name="minPrix"]').value;
   const maxPrixInput = document.querySelector('input[name="maxPrix"]').value;
-  const ouvertureInput = document.querySelector(
-    'input[name="ouverture"]'
-  ).value;
-  const fermetureInput = document.querySelector(
-    'input[name="fermeture"]'
-  ).value;
+  const ouvertureInput = document.querySelector('input[name="ouverture"]').value;
+  const fermetureInput = document.querySelector('input[name="fermeture"]').value;
   const etatInput = document.querySelector('select[name="etat"]').value;
   const trieInput = document.querySelector('select[name="trie"]').value;
   const statusInput = document.querySelector('input[name="status"]').value;
   const noteInput = document.querySelector('input[name="note"]').value;
-  const inputNote = document.querySelector(
-    'input[name="inputnoteValue"]'
-  ).value;
+  const inputNote = document.querySelector('input[name="inputnoteValue"]').value;
 
-  const categoryInputs = Array.from(
-    document.querySelectorAll('input[name="nomcategorie[]"]:checked')
-  ).map((input) => input.value);
-  const optionInputs = Array.from(
-    document.querySelectorAll('input[name="option[]"]:checked')
-  ).map((input) => input.value);
+  const categoryInputs = Array.from(document.querySelectorAll('input[name="nomcategorie[]"]:checked')).map((input) => input.value);
+  const optionInputs = Array.from(document.querySelectorAll('input[name="option[]"]:checked')).map((input) => input.value);
 
   const params = new URLSearchParams();
   if (searchInput) params.append("titre", searchInput);
@@ -38,8 +26,7 @@ function rechercher() {
   if (statusInput) params.append("status", statusInput);
   if (noteInput) params.append("note", noteInput);
   if (inputNote) params.append("inputnote", inputNote);
-  if (categoryInputs.length > 0)
-    params.append("nomcategorie", categoryInputs.join(","));
+  if (categoryInputs.length > 0) params.append("nomcategorie", categoryInputs.join(","));
   if (optionInputs.length > 0) params.append("option", optionInputs.join(","));
 
   // Compter le nombre de filtres actifs
@@ -60,9 +47,7 @@ function rechercher() {
 
   // Afficher le nombre de filtres actifs
   const nombreFiltresActifs = filtreActifCount - 3;
-  document.getElementById(
-    "nombreFiltresActifs"
-  ).innerText = `Nombre de filtres actifs : ${nombreFiltresActifs < 0 ? 0 : nombreFiltresActifs}`;
+  document.getElementById("nombreFiltresActifs").innerText = `Nombre de filtres actifs : ${nombreFiltresActifs < 0 ? 0 : nombreFiltresActifs}`;
 
   const xhr = new XMLHttpRequest();
   xhr.open("GET", `listeOffres.php?${params.toString()}`, true);
@@ -73,19 +58,13 @@ function rechercher() {
         const response = JSON.parse(xhr.responseText);
         const resultatsContainer = document.getElementById("resultats");
         resultatsContainer.innerHTML = response.offres.join("");
-        document.getElementById(
-          "nombreOffres"
-        ).innerHTML = `Nombre d'offres affichées : ${response.nombreOffres}`;
+        document.getElementById("nombreOffres").innerHTML = `Nombre d'offres affichées : ${response.nombreOffres}`;
         applyStyles();
       } catch (e) {
         console.error("Erreur lors du traitement de la réponse JSON:", e);
       }
     } else {
-      console.error(
-        "Erreur lors de la requête AJAX:",
-        xhr.status,
-        xhr.statusText
-      );
+      console.error("Erreur lors de la requête AJAX:", xhr.status, xhr.statusText);
     }
   };
   xhr.send();
@@ -117,9 +96,7 @@ function changerStatus(nouveauStatus) {
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("searchForm");
   const searchInput = document.querySelector('input[name="titre"]');
-  const localisationInput = document.querySelector(
-    'input[name="localisation"]'
-  );
+  const localisationInput = document.querySelector('input[name="localisation"]');
   const minPrixInput = document.querySelector('input[name="minPrix"]');
   const maxPrixInput = document.querySelector('input[name="maxPrix"]');
   const ouvertureInput = document.querySelector('input[name="ouverture"]');
@@ -127,9 +104,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const etatInput = document.querySelector('select[name="etat"]');
   const trieInput = document.querySelector('select[name="trie"]');
   const statusInput = document.querySelector('input[name="status"]');
-  const categoryInputs = document.querySelectorAll(
-    'input[name="nomcategorie[]"]'
-  );
+  const categoryInputs = document.querySelectorAll('input[name="nomcategorie[]"]');
   const optionInputs = document.querySelectorAll('input[name="option[]"]');
   const noteInput = document.querySelector('input[name="note"]');
   const inputNote = document.querySelector('input[name="inputnoteValue"]');
